aa990352e9b65f65e277e57af0f6cef8a4221b20
Eric Hart <tug68170@temple.edu>
2019-11-12 17:38:06 -0500

    urgent notification implementation

--- smartTransit/app/src/main/java/temple/edu/smarttransit/MainActivity.java
+++ smartTransit/app/src/main/java/temple/edu/smarttransit/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,12 +25,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void DisplayNotification(View view){
 
+        // create full screen intent
+        Intent fullscreenIntent = new Intent();
+        PendingIntent fullscreenPendingIntent = PendingIntent.getActivity(this,
+                0, fullscreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         // build notification
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
         builder.setSmallIcon(R.drawable.ic_directions_subway_black_24dp);
         builder.setContentTitle("SmartTransit");
         builder.setContentText("BSL Southbound is arriving at Susquehanna-Dauphin Station in 10 minutes");
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText("BSL Southbound is arriving at " +
+                "Susquehanna-Dauphin Station in 10 minutes"));
         builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        builder.setFullScreenIntent(fullscreenPendingIntent, true);
 
         // display notification
         NotificationManagerCompat nf_mngr = NotificationManagerCompat.from(this);
