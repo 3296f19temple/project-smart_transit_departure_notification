cb6ffe0cf29a51e5ca58ef5a1210ca27ace63014
stevennardo <steven.nardo@temple.edu>
2019-11-13 12:19:25 -0500

    Added UI Files

--- app/src/main/java/com/example/transitnotification/MainActivity.java
+++ app/src/main/java/com/example/transitnotification/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.transitnotification;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Anonymous on click function that redirects to the bus selector activity
+        ImageView bus_img = (ImageView) findViewById(R.id.bus_icon);
+        bus_img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getBaseContext(), bus_selector.class);
+                startActivity(myIntent);
+            }
+        });
+
+        //Anonymous on click function that redirects to the subway selector activity
+        ImageView subway_img = (ImageView) findViewById(R.id.subway_icon);
+        subway_img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getBaseContext(), subway_selector.class);
+                startActivity(myIntent);
+            }
+        });
+
+        //Anonymous on click function that redirects to the rail selector activity
+        ImageView rail_img = (ImageView) findViewById(R.id.rail_icon);
+        rail_img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getBaseContext(), rail_selector.class);
+                startActivity(myIntent);
+            }
+        });
+
+        //Anonymous on click function that redirects to the favorites activity
+        ImageView favorites_img = (ImageView) findViewById(R.id.favorites_icon);
+        favorites_img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getBaseContext(), favorites.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/transitnotification/rail_selector.java
@@ -0,0 +1,25 @@
+package com.example.transitnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class rail_selector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rail_selector);
+
+        ImageView logo_img = (ImageView) findViewById(R.id.app_logo);
+        logo_img.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+}
